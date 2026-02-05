cb02533a299e84dfd3634481489a04bc975b37a6
Ahmed <bellahoptions@gmail.com>
2026-02-05 08:53:02 +0100

    Animate logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('contents')
     <main role="main">
         <div class="flex items-center justify-center min-h-screen">
-            <img src="{{ asset('/logo.svg') }}" class="h-10 md:h-20" alt="Alet Inspirationz Logo"/>
+            <img src="{{ asset('/logo.svg') }}" class="logo h-10 md:h-20" alt="Alet Inspirationz Logo"/>
         </div>
     </main>
 @endsection
